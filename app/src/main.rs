185def8b24f967f5b7c86b448dca318b426b8973
Scott Mathog <smathog@gmail.com>
2024-05-11 12:27:39 -0700

    Set up initial app endpoint

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello_world() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(hello_world))
+        .bind(("localhost", 8080))?
+        .run()
+        .await
 }
