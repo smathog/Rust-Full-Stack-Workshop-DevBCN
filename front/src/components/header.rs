a54d25cdeb9aa69b578428dbd4e85e59b20c8fc6
Scott Mathog <smathog@gmail.com>
2024-05-20 22:11:56 -0700

    Fix css

--- front/src/components/header.rs
+++ front/src/components/header.rs
@@ -1,15 +1,19 @@
 use dioxus::prelude::*;
+use shared::models::FilmModel;
 use crate::components::Button;
 use crate::models::{ButtonType, FilmModalVisibility};
 
 #[component]
 pub fn Header() -> Element {
     let mut modal_visibility = use_context::<Signal<FilmModalVisibility>>();
-
+    let films = use_signal::<Option<Vec<FilmModel>>>(|| None);
+    let selected_film = use_signal::<Option<FilmModel>>(|| None);
+    let force_get_films = use_signal(|| ());
     rsx! {
         header {
             class: "sticky top-0 z-10 text-gray-400 bg-blue-300 body-font shadow-md",
             div {
+                class: "container mx-auto flex flex-wrap p-0 flex-col md:flex-row justify-between items-center",
                 a {
                     class: "flex title-font font-medium items-center text-teal-950 mb-4",
                     img {
